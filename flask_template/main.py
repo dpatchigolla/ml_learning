4e758e9252a752c050f2dbdb9b115f6aa482903a
z003cbx <dileep.patchigolla@target.com>
2018-11-21 15:17:00 +0530

    Added type examples in flask file

--- flask_template/main.py
+++ flask_template/main.py
@@ -11,8 +11,10 @@ def home():
 def test(query):
     debug = request.args.get('debug',False)
     method = request.args.get('method','ql')
+    int_val = request.args.get('int_val', 1, type=int) # adding type to parse args to appropriate datatype
+    float_val = request.args.get('float_val', 0.1, type=float)
     assert query!='test'
-    resp = {'query':query}
+    resp = {'query':query, 'int_val':int_val, 'float_val':float_val}
     if debug:
         resp['debug'] = {'method':method}
     return jsonify(resp)
@@ -20,4 +22,4 @@ def test(query):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5015)
